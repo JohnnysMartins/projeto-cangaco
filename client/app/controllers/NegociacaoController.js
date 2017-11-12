4edd46ec28ab44bb0c027bfc4e6132a2cc67d114
JohnnysMartins <johnnys.adson@gmail.com>
2017-11-12 00:15:19 -0300

    Trabalhando com listas imutáveis no javascript, criando metodos para limpar a tela e para criar a negociacao (cap 5.3) blindando o nosso modelo

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,19 +5,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
-        let negociacao = new Negociacao(
+
+        let negociacao = this._criarNegociacao();
+        this._negociacoes.adiciona(negociacao);
+        this._limparFormulario();
+    }
+
+    _criarNegociacao(){
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        console.log(negociacao);
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
+    }
 
+    _limparFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0,0;
+        this._inputData.focus();
     }
 
 }
